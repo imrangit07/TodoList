beaf4a76d1c0fabc91d3b35d12dda05862343b4f
imrangit07 <imranhussain21309@gmail.com>
2025-03-10 15:37:23 +0530

    Update some code

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-let addBtn = document.querySelector("#add-btn");
 let allListItems = [
     {
         item: "Wake up early",
         date: '2025-03-09',
-        time:'06/00',
+        time: '06/00',
     },
     {
         item: "Exercise for 30 minutes",
         date: '2025-03-09',
-        time:'06/30',
+        time: '06/30',
     },
 ];
 
+
+
 printLists();
-addBtn.addEventListener("click", () => {
-    let inputDisplay = document.querySelector("#input-display");
-    let inputList = inputDisplay.value;
+document.querySelector("#add-btn")
+.addEventListener("click", () => {
     let date = document.querySelector("#input-date").value;
     let time = document.querySelector("#input-time").value;
-
+    let inputDisplay = document.querySelector("#input-display");
+    let inputList = inputDisplay.value;
 
     if (inputList == '') {
         alert("Please Enter Todo List");
@@ -27,14 +28,14 @@ addBtn.addEventListener("click", () => {
     } else if (date == "") {
         alert("please Select Date For Your List");
         return;
-    }else if (time == "") {
+    } else if (time == "") {
         alert("please Select Time For Your List");
         return;
     }
 
     console.log(inputList);
 
-    allListItems.push({ item: inputList, date: date, time:time});
+    allListItems.push({ item: inputList, date: date, time: time });
     inputDisplay.value = ``;
     inputDisplay.focus();
 
